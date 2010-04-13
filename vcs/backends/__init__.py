6ad51d9876b0f99e01533ccae1998144f6da6c7d
Marcin Kuzminski <marcin@python-blog.com>
2010-04-14 01:05:12 +0200

    Changed defaults of get tags and branches

---
+++ vcs/backends/__init__.py
@@ -0,0 +1,30 @@
+""" backends module"""
+from pprint import pformat
+from vcs.exceptions import VCSError
+from vcs.utils.imports import import_class
+
+BACKENDS = {
+    'hg': 'vcs.backends.hg.MercurialRepository',
+}
+
+def get_repo(alias, path):
+    """
+    Returns ``Repository`` object of type linked with given ``alias`` at
+    the specified ``path``.
+    """
+    backend = get_backend(alias)
+    repo = backend(path)
+    return repo
+
+def get_backend(alias):
+    """
+    Returns ``Repository`` class identified by the given alias or raises
+    VCSError if alias is not recognized or backend class cannot be imported.
+    """
+    if alias not in BACKENDS:
+        raise VCSError("Given alias '%s' is not recognized! Allowed aliases:\n"
+            "%s" % (alias, pformat(BACKENDS.keys())))
+    backend_path = BACKENDS[alias]
+    klass = import_class(backend_path)
+    return klass
+
