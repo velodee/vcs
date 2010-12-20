a28201153f760113655e2c9343e27d5002a4b399
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-12-20 01:42:29 +0100

    webvcs overall updates

--- vcs/webvcs/views/repository.py
+++ vcs/webvcs/views/repository.py
@@ -1,17 +1,22 @@
 import os
+import vcs
+
 from django.conf import settings
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from vcs.web.simplevcs.models import Repository
 from vcs.utils.helpers import get_scm
 
 abspath = lambda *p: os.path.abspath(os.path.join(*p))
 
-def repository_detail(request, path='', template_name='webvcs/repo.html'):
+def get_repo_for_request(request):
     alias, rel_path = get_scm(settings.CURDIR)
     repo_path = abspath(settings.CURDIR, rel_path)
+    repo = vcs.get_repo(repo_path, alias)
+    return repo
 
-    repo = Repository(path=repo_path, alias=alias)
+def repository_detail(request, path='', template_name='webvcs/repo.html'):
+    repo = get_repo_for_request(request)
 
     context = {
         'repo': repo,
@@ -19,10 +24,8 @@ def repository_detail(request, path='', template_name='webvcs/repo.html'):
     return render_to_response(template_name, context, RequestContext(request))
 
 def changeset_detail(request, changeset_id, template_name='webvcs/cs.html'):
-    alias, rel_path = get_scm(settings.CURDIR)
-    repo_path = abspath(settings.CURDIR, rel_path)
+    repo = get_repo_for_request(request)
 
-    repo = Repository(path=repo_path, alias=alias)
     changeset = repo.get_changeset(changeset_id)
 
     context = {
@@ -30,4 +33,24 @@ def changeset_detail(request, changeset_id, template_name='webvcs/cs.html'):
         'changeset': changeset,
     }
     return render_to_response(template_name, context, RequestContext(request))
+
+def node_detail(request, changeset_id, path, template_name='webvcs/node.html'):
+    repo = get_repo_for_request(request)
+    changeset = repo.get_changeset(changeset_id)
+    node = changeset.get_node(path)
+
+    context = {
+        'repo': repo,
+        'changeset': changeset,
+        'node': node,
+        'splitpath': node.path.split('/'),
+    }
+    return render_to_response(template_name, context, RequestContext(request))
+
+def node_raw(request, changeset_id, path):
+    repo = get_repo_for_request(request)
+    changeset = repo.get_changeset(changeset_id)
+    node = changeset.get_node(path)
+
+    return HttpResponse(node.content, mimetype=node.mimetype)
 
