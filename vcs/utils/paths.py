3c2c01aa4749923f8a13f10d9c48a2ecb45003cd
Marcin Kuzminski <marcin@python-works.com>
2011-01-20 22:56:08 +0100

    implemented #45 archival methods for easy archive file creation

---
+++ vcs/utils/paths.py
@@ -0,0 +1,29 @@
+import os
+
+abspath = lambda * p: os.path.abspath(os.path.join(*p))
+
+def get_dirs_for_path(*paths):
+    """
+    Returns list of directories, including intermediate.
+    """
+    for path in paths:
+        head = path
+        while head:
+            head, tail = os.path.split(head)
+            if head:
+                yield head
+            else:
+                # We don't need to yield empty path
+                break
+
+def get_dir_size(path):
+    root_path = path
+    size = 0
+    for path, dirs, files in os.walk(root_path):
+        for f in files:
+            try:
+                size += os.path.getsize(os.path.join(path, f))
+            except OSError:
+                pass
+    return size
+
