af6171bd8df549e80824e395962080ef7b263d3e
marcinkuzminski <marcinkuzminski@localhost>
2011-11-15 00:06:45 +0200

    fixed issue with diff filepaths, for new nodes there was /dev/null returned as filename

---
+++ vcs/conf/settings.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+abspath = lambda * p: os.path.abspath(os.path.join(*p))
+
+VCSRC_PATH = os.environ.get('VCSRC_PATH')
+
+if not VCSRC_PATH:
+    HOME_ = os.getenv('HOME',os.getenv('USERPROFILE',tempfile.gettempdir()))
+
+VCSRC_PATH = VCSRC_PATH or abspath(HOME_, '.vcsrc')
+
+BACKENDS = {
+    'hg': 'vcs.backends.hg.MercurialRepository',
+    'git': 'vcs.backends.git.GitRepository',
+}
+
+ARCHIVE_SPECS = {
+    'tar': ('application/x-tar', '.tar'),
+    'tbz2': ('application/x-bzip2', '.tar.bz2'),
+    'tgz': ('application/x-gzip', '.tar.gz'),
+    'zip': ('application/zip', '.zip'),
+}
